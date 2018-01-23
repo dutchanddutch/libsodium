ba6d85a57f65308a6aa7196bd0c09115d3d6a535
Matthijs van Duin <matthijsvanduin@gmail.com>
2018-01-23 14:59:03 +0100

    add configure option to use size_t for length params
    The use of unsigned long long is completely bogus.  Unfortunately fixing
    this is an incompatible API change, therefore it has to be explicitly
    requested via ./configure --enable-size_t
    
    A typedef has been added, sodium_size_t, to allow code to work with both
    variants of the API.  Users of libsodium should be encouraged to make
    use of it, to hopefully allow the default to change in the future.

--- msvc-scripts/process.bat
+++ msvc-scripts/process.bat
@@ -2,4 +2,6 @@ cscript msvc-scripts/rep.vbs //Nologo s/@VERSION@/1.0.16/ < src\libsodium\includ
 cscript msvc-scripts/rep.vbs //Nologo s/@SODIUM_LIBRARY_VERSION_MAJOR@/10/ < tmp > tmp2
 cscript msvc-scripts/rep.vbs //Nologo s/@SODIUM_LIBRARY_VERSION_MINOR@/1/ < tmp2 > src\libsodium\include\sodium\version.h
 del tmp tmp2
-cscript msvc-scripts/rep.vbs //Nologo s/@SODIUM_LIBRARY_MINIMAL_DEF@// < src\libsodium\include\sodium\export.h.in > src\libsodium\include\sodium\export.h
+cscript msvc-scripts/rep.vbs //Nologo s/@SODIUM_LIBRARY_MINIMAL_DEF@// < src\libsodium\include\sodium\export.h.in > tmp
+cscript msvc-scripts/rep.vbs //Nologo s/@SODIUM_LIBRARY_SIZE_T_DEF@// < tmp > src\libsodium\include\sodium\export.h
+del tmp
